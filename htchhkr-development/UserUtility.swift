c82567ea60a413440805c41977c9ce656fe2929b
Abdulmajeed Albani <a.albani@morniksa.com>
2018-11-06 23:43:15 +0300

    add user utility class

---
+++ htchhkr-development/UserUtility.swift
@@ -0,0 +1,9 @@
+//
+//  UserUtility.swift
+//  htchhkr-development
+//
+//  Created by Abdulmajeed khalid on 06/11/2018.
+//  Copyright © 2018 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
